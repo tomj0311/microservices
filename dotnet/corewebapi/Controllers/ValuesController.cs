6ffd086dad0b6efaa18c6b07de2e8871b1c9a4ee
Tom Jose <tom.jose@quest-global.com>
2019-07-01 04:03:17 +0530

    dotnet spring boot api call

---
+++ dotnet/corewebapi/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+
+namespace corewebapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            HttpClient httpClient = new HttpClient();
+            var response = httpClient.GetStringAsync("http://localhost:8080/greeting").Result;
+
+            return new string[] { response.ToString() };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ python/app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+app = Flask(__name__)
+
+
+quarks = [{'name': 'up', 'charge': '+2/3'},
+          {'name': 'down', 'charge': '-1/3'},
+          {'name': 'charm', 'charge': '+2/3'},
+          {'name': 'strange', 'charge': '-1/3'}]
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return jsonify({'message' : 'Hello, World!'})
+
+@app.route('/quarks', methods=['GET'])
+def returnAll():
+    return jsonify({'quarks' : quarks})
+
+@app.route('/quarks/<string:name>', methods=['GET'])
+def returnOne(name):
+    theOne = quarks[0]
+    for i,q in enumerate(quarks):
+      if q['name'] == name:
+        theOne = quarks[i]
+    return jsonify({'quarks' : theOne})
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
