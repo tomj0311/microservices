6b5998f3fe919c0fe97717e7c121ccb046b1cacf
tomj0311 <tomj0311@gmail.com>
2020-04-02 07:55:27 +0530

    SDL Wasm tests

---
+++ c-cpp/sockets/beast/server-async.cpp
@@ -0,0 +1,15 @@
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <thread>
+
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace websocket = beast::websocket;
+namespace net = boost::asio;
+using tcp = boost::asio::ip::tcp;
+
